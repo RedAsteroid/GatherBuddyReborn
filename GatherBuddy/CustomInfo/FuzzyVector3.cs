d0c4551edfa2a368eff461d9b67f440eb61b3f55
RedAsteroid <123588653+RedAsteroid@users.noreply.github.com>
2024-11-13 22:23:21 +0800

    Update MoveToCloseNode method to prevent infinite navigation loops 尝试修复移动逻辑导致的无限重复寻路问题

--- GatherBuddy/CustomInfo/FuzzyVector3.cs
+++ GatherBuddy/CustomInfo/FuzzyVector3.cs
@@ -6,6 +6,8 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using ECommons.GameHelpers;
+using static FFXIVClientStructs.FFXIV.Client.Game.Character.VfxContainer;
+using ECommons.MathHelpers;
 
 namespace GatherBuddy.CustomInfo
 {
@@ -68,6 +70,13 @@ namespace GatherBuddy.CustomInfo
         public static float DistanceToPlayer(this Vector3 vector)
         {
             var distance = Vector3.Distance(vector, Player.Object.Position);
+            return distance;
+        }
+
+        public static float DistanceToPlayer2(this Vector3 vector)
+        {
+            var vector2 = vector.ToVector2();
+            var distance = Vector2.Distance(vector2, Player.Object.Position.ToVector2());
             return distance;
         }
     }
